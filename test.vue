299a477a6e793362a6252372c9d09e3082c6ddd0
岸直哉 <user.id.125125@gmail.com>
2020-11-02 08:17:08 +0900

    反転

---
+++ test.vue
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <div id="app">{{message}}</div>
+    <button v-on:click=reverseMessage>反転</button>
+
+
+    <script>
+        new Vue ({
+            el: "#app",
+            data: {
+                message: "hello world"
+            },
+            methods: {
+                reverseMessage: function() {
+                    this.message = this.message.split('').reverse().join('')
+                }
+            }
+        })
+    </script>
+</body>
+</html>
