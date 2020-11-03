0721d01b5ec84760963343eafcb9bb97b746050f
岸直哉 <user.id.125125@gmail.com>
2020-11-04 07:25:01 +0900

    アラート

--- test.vue
+++ test.vue
@@ -1,28 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-    <div id="app">{{message}}</div>
-    <button v-on:click=reverseMessage>反転</button>
+<template>
+    <div id="app">
+        <p>{{number}}回クリック</p>
+        <button v-on:click="plus">クリック</button>
+        <p v-on:mousemove="change">aaaaaaaaaaaaaa
+            <span v-on:mousemove.stop>jjjjjjjjjjjjjjjjjj</span>
+        </p>
+        <p>x:{{x}} y:{{y}}</p>
 
+        <input type="text" @keydown.enter="myAlert">
+    </div>
+</template>
 
-    <script>
+<script>
+Vue.config.devtools = true;
+
         new Vue ({
             el: "#app",
             data: {
-                message: "hello world"
+                number: 0,
+                x: 0,
+                y: 0,
             },
             methods: {
-                reverseMessage: function() {
-                    this.message = this.message.split('').reverse().join('')
+                plus: function() {
+                    this.number = this.number + 1
+                },
+                change: function(event) {
+                    this.x = event.clientX;
+                    this.y = event.clientY;
+                },
+                myAlert: function () {
+                    alert("アラート");
                 }
             }
         })
     </script>
-</body>
-</html>
