1d04303a7538cd3f11d2cd777807977b283fe979
岸直哉 <user.id.125125@gmail.com>
2020-12-25 07:46:41 +0900

    baseinput 途中

--- practice/src/App.vue
+++ practice/src/App.vue
@@ -1,63 +1,29 @@
 <template>
 <div>
-  <LikeHeader>
-    <template #header>
-      <h1>わっしょい</h1>
-    </template>
-    <template #body>
-      <h1>わっしょいわっしょい</h1>
-    </template>
-    <template #footer>
-      <h1>わっしょいわっしょいわっしょい</h1>
-    </template>
-  </LikeHeader>
-  <hr>
-
-
-<input type="text" v-model="vali" value="">
-
-<div :v-for="(item, i) in language" :key="i">
-
-  <input type="checkbox" :id="item" :value="item" v-model="li">
-  <label :for="item">{{item}}</label>
-  {{li}}
-</div>
+  <BaseInput 
+  :value="eventData.title"
+  @input="evetData.title = $event"
+  ></BaseInput>
 </div>
 </template>
 
 
 <script>
-import LikeHeader from './components/LikeHeader'
+import BaseInput from './components/BaseInput'
+
     
 export default {
-  components: {LikeHeader},
+  components: {BaseInput},
   data() {
     return {
-      language: [
-        'Java',
-        'PHP',
-        'Python',
-        'Ruby',
-      ],
-      num: 0,
-      vali: '',
-      li: []
+      
     }
   },
   watch: {
-    vali(val) {
-      let check = /^([1-9]\d*|0)$/
-        if (check.test(val)) {
-          this.vali = val
-        } else {
-          this.vali = val.slice(0,-1)
-        }
-    }
+    
   },
   methods: {
-    func (val) {
-      this.num = val
-    }
+    
   }
 }
 </script>
---
+++ practice/src/components/BaseInput.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <input 
+    type="text"
+    id="title"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+    >
+    
+</div>
+</template>
+
+<script>
+export default {
+    props: ['value']
+    
+}
+</script>
+
+<style scoped>
+
+</style>
