fc32dd3c3be47b9ae66f873b7084b5d543e57cb7
岸直哉 <user.id.125125@gmail.com>
2020-12-26 11:10:04 +0900

    custom directive

--- practice/src/App.vue
+++ practice/src/App.vue
@@ -4,6 +4,8 @@
   :value="eventData"
   @input="eventData = $event"
   ></BaseInput>
+
+  <p v-border>ふぁkjfかsjdfkじゃs</p>
 </div>
 </template>
 
--- practice/src/main.js
+++ practice/src/main.js
@@ -6,6 +6,11 @@ import LikeNumber from './components/LikeNumber.vue'
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber)
 
+Vue.directive("border", function(el, binding){
+  el.style.border = "solid red 2px"
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
