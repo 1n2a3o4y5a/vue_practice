0d682aaa4565f7fcb5a0df04b1648cab93f81f08
岸直哉 <user.id.125125@gmail.com>
2021-01-20 00:05:56 +0900

    checkbox

---
+++ practice/src/components/CheckBox.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <input 
+        type="checkbox"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+        >
+        <p>{{value}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['value']
+    
+}
+</script>
+
+<style scoped>
+
+</style>
